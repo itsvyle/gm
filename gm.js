121279db628daa0d3ec845ce18f6ec4da43a4e6c
itsvyle <65409906+itsvyle@users.noreply.github.com>
2021-01-15 15:49:48 -0800

    Update gm.js

--- gm.js
+++ gm.js
@@ -66,6 +66,22 @@
 				}
 			});
 		}
+		
+		if (constructor &&
+			constructor.prototype &&
+			constructor.prototype.children == null) {
+			Object.defineProperty(constructor.prototype, 'children', {
+			  get: function() {
+				let i = 0, node, nodes = this.childNodes, children = [];
+				while (node = nodes[i++]) {
+				  if (node.nodeType === 1) {
+					children.push(node);
+				  }
+				}
+				return children;
+			  }
+			});
+		  }
 	})(window.Node || window.Element);
 
 	// =========================== STRING/ARRAY .INCLUDES() ===========================
