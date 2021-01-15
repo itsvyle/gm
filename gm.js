5ae821f900f641292032e54bf000ee910f120858
itsvyle <65409906+itsvyle@users.noreply.github.com>
2021-01-15 15:37:23 -0800

    Update gm.js

--- gm.js
+++ gm.js
@@ -35,6 +35,22 @@
 		return this.indexOf(searchString, position) === position;
 	  };
 	}
+	// =========================== NODE CHILD ELEMENT COUNT ===========================
+	(function(constructor) {
+	  if (constructor &&
+		  constructor.prototype &&
+		  constructor.prototype.childElementCount == null) {
+		Object.defineProperty(constructor.prototype, 'childElementCount', {
+		  get: function() {
+			var i = 0, count = 0, node, nodes = this.childNodes;
+			while (node = nodes[i++]) {
+			  if (node.nodeType === 1) count++;
+			}
+			return count;
+		  }
+		});
+	  }
+	})(window.Node || window.Element);
 
 	// =========================== STRING/ARRAY .INCLUDES() ===========================
 	if (!String.prototype.includes) {
