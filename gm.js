7a7b8933064ca766ef6c4dbebfe4fb97fdc382c2
itsvyle <65409906+itsvyle@users.noreply.github.com>
2021-01-28 11:04:56 -0800

    Update gm.js

--- gm.js
+++ gm.js
@@ -64,21 +64,33 @@
 		  };
 		};
 	// =========================== NODE.REPLACEWITH() ===========================
-		(function (arr) {
-		  arr.forEach(function (item) {
-			if (item.hasOwnProperty('replaceWith')) {
-			  return;
-			}
-			Object.defineProperty(item, 'replaceWith', {
-			  configurable: true,
-			  enumerable: true,
-			  writable: true,
-			  value: function replaceWith(node) {
-				this.parentNode.replaceChild(node, this.parentNode);
-			  }
-			});
-		  });
-		})([Element.prototype, CharacterData.prototype, DocumentType.prototype]);
+	var ReplaceWithPolyfill = function () {
+	  'use-strict'; // For safari, and IE > 10
+	  var parent = this.parentNode, i = arguments.length, currentNode;
+	  if (!parent) return;
+	  if (!i) // if there are no arguments
+	    parent.removeChild(this);
+	  while (i--) { // i-- decrements i and returns the value of i before the decrement
+	    currentNode = arguments[i];
+	    if (typeof currentNode !== 'object'){
+	      currentNode = this.ownerDocument.createTextNode(currentNode);
+	    } else if (currentNode.parentNode){
+	      currentNode.parentNode.removeChild(currentNode);
+	    }
+	    // the value of "i" below is after the decrement
+	    if (!i) // if currentNode is the first argument (currentNode === arguments[0])
+	      parent.replaceChild(currentNode, this);
+	    else // if currentNode isn't the first
+	      parent.insertBefore(currentNode, this.nextSibling);
+	  }
+	}
+	if (!Element.prototype.replaceWith)
+	    Element.prototype.replaceWith = ReplaceWithPolyfill;
+	if (!CharacterData.prototype.replaceWith)
+	    CharacterData.prototype.replaceWith = ReplaceWithPolyfill;
+	if (!DocumentType.prototype.replaceWith)
+	    DocumentType.prototype.replaceWith = ReplaceWithPolyfill;
+
 	
     // =========================== OBJECT.VALUES ===========================
     (function () {
