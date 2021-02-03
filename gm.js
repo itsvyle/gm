decd241d8e6f17f71ad7795cd993ca3818dfd3fc
itsvyle <65409906+itsvyle@users.noreply.github.com>
2021-02-03 13:01:23 -0800

    Update gm.js

--- gm.js
+++ gm.js
@@ -27,6 +27,38 @@
 	  });
 	})([Element.prototype, CharacterData.prototype, DocumentType.prototype]);
 
+    //  Yes, it does work with `new (funcA.bind(thisArg, args))`
+    if (!Function.prototype.bind) (function(){
+        var ArrayPrototypeSlice = Array.prototype.slice;
+        Function.prototype.bind = function(otherThis) {
+            if (typeof this !== 'function') {
+            // closest thing possible to the ECMAScript 5
+            // internal IsCallable function
+            throw new TypeError('Function.prototype.bind - what is trying to be bound is not callable');
+            }
+
+            var baseArgs= ArrayPrototypeSlice.call(arguments, 1),
+                baseArgsLength = baseArgs.length,
+                fToBind = this,
+                fNOP    = function() {},
+                fBound  = function() {
+                baseArgs.length = baseArgsLength; // reset to default base arguments
+                baseArgs.push.apply(baseArgs, arguments);
+                return fToBind.apply(
+                        fNOP.prototype.isPrototypeOf(this) ? this : otherThis, baseArgs
+                );
+                };
+
+            if (this.prototype) {
+            // Function.prototype doesn't have a prototype property
+            fNOP.prototype = this.prototype;
+            }
+            fBound.prototype = new fNOP();
+
+            return fBound;
+        };
+    })();
+
     // =========================== ARRAY.indexObject ===========================
     if (!Array.prototype.indexObject) {
         Array.prototype.indexObject = function (item,fields) {
@@ -502,6 +534,9 @@ window.addEventListener("load",gm.onDocLoad);
 					r.status = 0;
 					r.error_level = 1;
 					r.error = "Error making request(" + String(this.status) + ": " + this.responseText + ")";
+                    if (opts.json == true) {r.res = gm.JSONParse(this.responseText);} else {
+                        r.res = this.responseText;
+                    }
 				} else {
 					r.status = 1;
 					if (opts.json === true) {
