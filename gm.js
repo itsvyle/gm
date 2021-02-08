10b8513d38cf7ff92f27fb3123c6d74948fd2cd6
itsvyle <65409906+itsvyle@users.noreply.github.com>
2021-02-08 09:19:55 -0800

    Update gm.js

--- gm.js
+++ gm.js
@@ -58,7 +58,24 @@
         });
     }
 
-
+	if (!Function.prototype.bind) (function(){
+	  var slice = Array.prototype.slice;
+	  Function.prototype.bind = function() {
+	    var thatFunc = this, thatArg = arguments[0];
+	    var args = slice.call(arguments, 1);
+	    if (typeof thatFunc !== 'function') {
+	      // closest thing possible to the ECMAScript 5
+	      // internal IsCallable function
+	      throw new TypeError('Function.prototype.bind - ' +
+		     'what is trying to be bound is not callable');
+	    }
+	    return function(){
+	      var funcArgs = args.concat(slice.call(arguments))
+	      return thatFunc.apply(thatArg, funcArgs);
+	    };
+	  };
+	})();
+	/*
     //  Yes, it does work with `new (funcA.bind(thisArg, args))`
     if (!Function.prototype.bind) (function(){
         var ArrayPrototypeSlice = Array.prototype.slice;
@@ -89,7 +106,7 @@
 
             return fBound;
         };
-    })();
+    })();*/
 
     // =========================== ARRAY.indexObject ===========================
     if (!Array.prototype.indexObject) {
