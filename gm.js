eb7981643dd310e0d57cca01e2e71c8fe875b4cb
itsvyle <65409906+itsvyle@users.noreply.github.com>
2021-02-08 09:08:28 -0800

    Update gm.js

--- gm.js
+++ gm.js
@@ -27,6 +27,38 @@
 	  });
 	})([Element.prototype, CharacterData.prototype, DocumentType.prototype]);
 
+    // =========================== OBJECT.ASSIGN() ===========================
+    if (typeof(Object.assign) !== 'function') {
+    // Must be writable: true, enumerable: false, configurable: true
+        Object.defineProperty(Object, "assign", {
+            value: function assign(target, varArgs) { // .length of function is 2
+            'use strict';
+            if (target === null || target === undefined) {
+                throw new TypeError('Cannot convert undefined or null to object');
+            }
+
+            var to = Object(target);
+
+            for (var index = 1; index < arguments.length; index++) {
+                var nextSource = arguments[index];
+
+                if (nextSource !== null && nextSource !== undefined) {
+                for (var nextKey in nextSource) {
+                    // Avoid bugs when hasOwnProperty is shadowed
+                    if (Object.prototype.hasOwnProperty.call(nextSource, nextKey)) {
+                    to[nextKey] = nextSource[nextKey];
+                    }
+                }
+                }
+            }
+            return to;
+            },
+            writable: true,
+            configurable: true
+        });
+    }
+
+
     //  Yes, it does work with `new (funcA.bind(thisArg, args))`
     if (!Function.prototype.bind) (function(){
         var ArrayPrototypeSlice = Array.prototype.slice;
